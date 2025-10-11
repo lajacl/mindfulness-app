3c58f323d330ac85d66ccd000dc23266d5d891a6
lajacl <lavette.clifton@gmail.com>
2025-10-11 04:06:46 -0400

    Display list of text exercises and show guide on tap

---
+++ lib/data.dart
@@ -0,0 +1,91 @@
+final List<Map<String, String>> textExercises = [
+  {
+    "title": "Body Scan Meditation",
+    "text":
+        "Lie or sit comfortably, with your arms and legs relaxed.\n\n"
+        "Direct your attention to your feet, noticing any sensations like pressure, temperature, or itches.\n\n"
+        "Gradually move your awareness: to your lower legs, upper legs, abdomen, chest, and other parts of your body, observing any feelings without judgment.\n\n"
+        "Allow your mind to wander: and then gently bring your attention back to the body part you're scanning.\n\n"
+        "If you feel tension, acknowledge it and then let your attention move on to the next part of your body.",
+  },
+  {
+    "title": "Guided Breathing",
+    "text":
+        "Sit in a comfortable, upright position: with your eyes closed or your gaze soft.\n\n"
+        "Take a few deep breaths, noticing how your body relaxes.\n\n"
+        "Allow your breath to settle into a natural rhythm: and focus on the sensations of the breath as it flows in and out.\n\n"
+        "When thoughts arise, gently acknowledge them: and bring your attention back to your breath, letting thoughts pass like waves. ",
+  },
+  {
+    "title": "Mindful Walking",
+    "text":
+        "Find a quiet place 10 to 20 feet in length to walk, such as a straight line or a peaceful outdoor area.\n\n"
+        "Walk slowly and deliberately, focusing on the sensation of your feet touching the ground.\n\n"
+        "Pay attention to your body's subtle movements that keep your balance and posture as you walk.\n\n"
+        "When you reach the end of your path, turn and continue walking, maintaining awareness of your sensations.",
+  },
+  {
+    "title": "Mindful Eating",
+    "text":
+        "Before eating, take a moment to appreciate your food: by looking at it, smelling it, and observing its colors and textures.\n\n"
+        "As you eat, focus on the experience: of tasting, chewing, and swallowing, paying attention to the flavors and sensations in your mouth.\n\n"
+        "This can be done with any meal or even with a single piece of fruit.",
+  },
+  {
+    "title": "Mindful Break",
+    "text":
+        "Step away from your computer or phone to notice sounds, your heart rate, and the sensations in your body.",
+  },
+  {
+    "title": "Five senses exercise",
+    "text":
+        "Look around you and bring your attention to five things that you can see. Pick something that you don’t normally notice, like a shadow or a small crack in the concrete.\n\n"
+        "Bring awareness to four things that you are currently feeling, like the texture of your pants, the feeling of the breeze on your skin, or the smooth surface of a table you are resting your hands on.\n\n"
+        "Take a moment to listen, and note three things that you hear in the background. This can be the chirp of a bird, the hum of the refrigerator, or the faint sounds of traffic from a nearby road.\n\n"
+        "Bring your awareness to two smells that you usually filter out, whether they’re pleasant or unpleasant. Perhaps the breeze is carrying a whiff of pine trees if you’re outside, or the smell of a fast-food restaurant across the street.\n\n"
+        "Focus on one thing that you can taste right now, at this moment. You can take a sip of a drink, chew a piece of gum, eat something, notice the current taste in your mouth, or even open your mouth to search the air for a taste.",
+  },
+  {
+    "title": "Breathing meditation",
+    "text":
+        "Find a comfortable seated or lying position and close your eyes.\n\n"
+        "Focus on the physical sensations of your breath as it enters and leaves your body, such as the feeling in your nostrils or the rise and fall of your abdomen.\n\n"
+        "Don't force your breath; simply observe it.\n\n"
+        "When your mind wanders, gently guide your attention back to your breath without judgment. ",
+  },
+  {
+    "title": "Mindful Seeing",
+    "text":
+        "Find a space at a window where there are sights to be seen outside\n\n"
+        "Look at everything there is to see. Avoid labeling and categorizing what you see outside the window; instead of thinking “bird” or “stop sign,” try to notice the colors, the patterns, or the textures.\n\n"
+        "Pay attention to the movement of the grass or leaves in the breeze. Notice the many different shapes present in this small segment of the world you can see. Try to see the world outside the window from the perspective of someone unfamiliar with these sights.\n\n"
+        "Be observant, but not critical. Be aware, but not fixated.\n\n"
+        "If you become distracted, gently pull your mind away from those thoughts and notice a color or shape again to put you back in the right frame of mind.",
+  },
+  {
+    "title": "Sitting meditation",
+    "text":
+        "Sit comfortably with your back straight, feet flat on the floor and hands in your lap.\n\n"
+        "Breathing through your nose, focus on your breath moving in and out of your body.\n\n"
+        "If physical sensations or thoughts interrupt your meditation, note the experience and then return your focus to your breath.",
+  },
+  {
+    "title": "The Observer Meditation",
+    "text":
+        "Take a comfortable seated position, and listen to the script\n\n"
+        "Let yourself settle into your body and your mind.\n\n"
+        "Try to let go of thoughts and clear the mind of its usual considerations.\n\n"
+        "Focus your attention first to the room you’re sitting in. Picture yourself from the outside as you sit, exactly as an outsider might. Next, shift your attention inwards into your skin. Try and feel your skin as you are sitting in the chair.\n\n"
+        "Try to envision the shape that your skin is making as you sit in contact with the chair, shifting your awareness toward any physical sensations you are experiencing. As you feel each one, acknowledge its existence before letting your consciousness let go of it and move on naturally.\n\n"
+        "If you find any emotions coming up, recognize them and create space for them. Then bring your attention back to your observing self—your feelings and thoughts are there, but you are separate from them, noticing them. This is the “Observer you”.",
+  },
+  {
+    "title": "The 3-Step Mindfulness Exercise",
+    "text":
+        "Try to pause and take a comfortable but dignified posture. Notice the thoughts that come up and acknowledge your feelings, but let them pass. Attune yourself to who you are and your current state.\n\n"
+        "The goal is to focus attention on one thing: your breath. Be aware of the movement of your body with each breath, of how your chest rises and falls, how your belly pushes in and out, and how your lungs expand and contract. Find the pattern of your breath and anchor yourself to the present with this awareness.\n\n"
+        "Allow the awareness to expand out to your body. Notice the sensations you are experiencing, like tightness, aches, or perhaps a lightness in your face or shoulders. Keep in mind your body as a whole, as a complete vessel for your inner self.\n\n"
+        "If you wish, you can then expand your awareness even further to the environment around you. Bring your attention to what is in front of you. Notice the colors, shapes, patterns, and textures of the objects you can see. Be present at this moment, in your awareness of your surroundings.\n\n"
+        "When you are ready to finish the exercise, open your eyes slowly and try to carry that mindfulness with you as you go about your day.",
+  },
+];
--- lib/exercises.dart
+++ lib/exercises.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import 'package:mindfulness_app/data.dart';
 
 class ExercisesPage extends StatefulWidget {
   const ExercisesPage({super.key});
@@ -10,12 +10,21 @@ class ExercisesPage extends StatefulWidget {
 
 class _ExercisesPageState extends State<ExercisesPage>
     with TickerProviderStateMixin {
+  Map<String, String>? _selectedItem;
+  bool _itemSelected = false;
   late TabController _nestedConrtoller;
+
+  void _updateSelection(item) {
+    setState(() {
+      _itemSelected = true;
+      _selectedItem = item;
+    });
+  }
 
   @override
   void initState() {
     super.initState();
-    _nestedConrtoller = new TabController(length: 2, vsync: this);
+    _nestedConrtoller = TabController(length: 2, vsync: this);
   }
 
   @override
@@ -26,10 +35,34 @@ class _ExercisesPageState extends State<ExercisesPage>
 
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+
     return Column(
       mainAxisAlignment: MainAxisAlignment.start,
       children: <Widget>[
-        Image.asset('assets/images/mindful.jpg'),
+        _itemSelected
+            ? SizedBox(
+                height: screenHeight * 0.25,
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                    child: Column(
+                      children: [
+                        Text(
+                          '${_selectedItem!['title']!}\n',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        Text(_selectedItem!['text']!),
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            : Image.asset(
+                'assets/images/mindful.jpg',
+                height: screenHeight * 0.25,
+                fit: BoxFit.cover,
+              ),
         TabBar(
           controller: _nestedConrtoller,
           tabs: [
@@ -42,7 +75,16 @@ class _ExercisesPageState extends State<ExercisesPage>
             controller: _nestedConrtoller,
             children: [
               Tab(icon: Icon(Icons.video_library), text: 'Video', height: 100),
-              ListView(),
+              ListView.builder(
+                itemCount: textExercises.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(textExercises[index]["title"]!),
+                    trailing: Icon(Icons.chevron_right),
+                    onTap: () => _updateSelection(textExercises[index]),
+                  );
+                },
+              ),
             ],
           ),
         ),
