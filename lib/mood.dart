b0a88e18ccd4c697c85cd2b6c4812b7e46d30f4f
lajacl <lavette.clifton@gmail.com>
2025-10-11 18:52:46 -0400

    Add mood page with date and labeled image options

---
+++ lib/mood.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class MoodPage extends StatefulWidget {
+  const MoodPage({super.key});
+
+  @override
+  State<MoodPage> createState() => _MoodPageState();
+}
+
+class _MoodPageState extends State<MoodPage> {
+  String _getDate() {
+    DateTime datetime = DateTime.now();
+    return DateFormat('MMMM d, yyyy').format(datetime);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              _getDate(),
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+            ),
+          ),
+          Text(
+            'How are you?',
+            style: TextStyle(fontSize: 20),
+            textAlign: TextAlign.left,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  IconButton(
+                    icon: Icon(
+                      Icons.sentiment_very_satisfied,
+                      size: screenWidth / 4,
+                    ),
+                    onPressed: null,
+                  ),
+                  Text('Good'),
+                ],
+              ),
+              Column(
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.sentiment_neutral, size: screenWidth / 4),
+                    onPressed: null,
+                  ),
+                  Text('Okay'),
+                ],
+              ),
+              Column(
+                children: [
+                  IconButton(
+                    icon: Icon(
+                      Icons.sentiment_very_dissatisfied,
+                      size: screenWidth / 4,
+                    ),
+                    onPressed: null,
+                  ),
+                  Text('Bad'),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
